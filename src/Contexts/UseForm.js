cc2c530d3f4ec8485592b6e29b08303e648ad031
JoaoAlgeri <joaoalgeri@gmail.com>
2023-04-24 06:02:17 -0300

    First commit: Layout added

---
+++ src/Contexts/UseForm.js
@@ -0,0 +1,15 @@
+
+import { useState } from "react";
+
+function UseForm(initState={}){
+    const [form,setForm]=useState(initState)
+    
+    function handleForm({target:{value,name}}){
+        setForm({
+            ...form,
+            [name]: value,
+        })
+}
+return [form,handleForm]
+}
+export {UseForm};
